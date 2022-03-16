84456ef1f2129cab687849c37328a7b84b3b4000
Felix Chacaltana <felix.chacaltana.felix@gmail.com>
2022-03-16 10:44:39 -0500

    Organize files

---
+++ Pilot/Classes/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Pilot
+//
+//  Created by Felix Chacaltana on 16/03/22.
+//
+
+import Foundation
---
+++ Pilot/Classes/Pilot.swift
@@ -0,0 +1,8 @@
+//
+//  Pilog.swift
+//  Pilot
+//
+//  Created by Felix Chacaltana on 16/03/22.
+//
+
+import Foundation
